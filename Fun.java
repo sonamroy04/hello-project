531b9311c5a74240ae0894b39b59e5d046502fae
Sonam <78811627+sonamroy04@users.noreply.github.com>
2022-07-28 00:10:03 +0530

    Add files via upload

---
+++ Fun.java
@@ -0,0 +1,19 @@
+public class Fun{
+
+int a;String name;
+	  Fun()
+	{
+		a=2; 
+		name=null;
+	}
+	void show()
+	{
+		System.out.println(a+" "+name);
+		System.out.println("for me coding is like fun...!!!");
+	}
+public static void main(String[] args) {
+		
+			A r=new A();
+			r.show();
+		}
+	}
